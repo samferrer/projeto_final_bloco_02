79d2c78db017daae841f399c59d92de1b00d46ad
Samara Ferreira <samaraferreira.costa@hotmail.com>
2026-04-16 15:46:10 -0300

    Feature: busca por nome com LIKE e tratamento de erro

--- src/categoria/categoria.service.ts
+++ src/categoria/categoria.service.ts
@@ -1,44 +1,50 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Categoria } from "./entities/categoria.entity";
-import { Repository } from "typeorm";
-
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Categoria } from './entities/categoria.entity';
+import { Repository, Like } from 'typeorm';
 
 @Injectable()
 export class CategoriaService {
 
-    constructor(
-        @InjectRepository(Categoria)
-        private categoriaRepository:Repository<Categoria>,
-    ) {}
+  constructor(
+    @InjectRepository(Categoria)
+    private categoriaRepository: Repository<Categoria>,
+  ) {}
 
-    findAll() {
+  findAll() {
     return this.categoriaRepository.find();
   }
 
-  findOne(id: number) {
-    return this.categoriaRepository.findOneBy({ id });
+  async findOne(id: number) {
+    const categoria = await this.categoriaRepository.findOneBy({ id });
+
+    if (!categoria) {
+      throw new NotFoundException('Categoria não encontrada');
+    }
+
+    return categoria;
   }
 
   findByNome(nome: string) {
-  return this.categoriaRepository.find({
-    where: {
-      nome: nome
-    }
-  });
+    return this.categoriaRepository.find({
+      where: {
+        nome: Like(`%${nome}%`)
+      }
+    });
   }
 
   create(categoria: Categoria) {
     return this.categoriaRepository.save(categoria);
   }
 
-  update(id: number, categoria: Categoria) {
+  async update(id: number, categoria: Categoria) {
+    await this.findOne(id);
     categoria.id = id;
     return this.categoriaRepository.save(categoria);
   }
 
-  remove(id: number) {
+  async remove(id: number) {
+    await this.findOne(id);
     return this.categoriaRepository.delete(id);
   }
-
 }
